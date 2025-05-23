14dea9c2a41106eccc11034e9bfb0603c39039de
da-ssl <101528576+da-ssl@users.noreply.github.com>
2025-05-23 14:09:34 +0200

    upd

--- bdftojson_2.py
+++ bdftojson_2.py
@@ -1,19 +1,13 @@
-import numpy as np
 import binascii, json
 file = open("font.bdf", "r")
 lines = file.readlines()
 fontname = input("Font-Name:")
-maxCharLength = int(input("Max. Charlength:"))
+space = input("Zwischenraum:")
 output = {}
 mode = ""
 cchar = ""
 ccharlength = 0
-ccharlength_bbx= 0
 cchardata = []
-
-
-# Initialize 2D array with zeros
-cchardata2D = np.zeros((12, maxCharLength+1))
 
 for i in lines:
     if i.startswith("STARTCHAR"):
@@ -22,12 +16,11 @@ for i in lines:
 
     if i.startswith("BBX"):
         v = i.split(" ")
-        ccharlength_bbx = int(v[1])
+        ccharlength = v[1]
         continue
 
     if i.startswith("BITMAP"):
         mode = "writeChar"
-        row = 0
         continue
 
     if mode == "readChar":
@@ -36,22 +29,31 @@ for i in lines:
             cchar = cchar[:-1]
 
     if i.startswith("ENDCHAR"):
-        output[cchar] = cchardata2D
-        cchardata2D = np.zeros((ccharlength_bbx, maxCharLength))
+        output[cchar] = cchardata
+        cchardata = []
+        ccharlength = 0
+        
 
     if mode == "writeChar":
         if i.startswith("ENDCHAR"):
             mode = ""
             continue
-        
-        # Convert hexadecimal to binary
-        integer = int(i, 16)
-        currentCharLine = f'{integer:0>{maxCharLength}b}'
-        for col in range(len(currentCharLine)):
-            cchardata2D[row][col] = int(currentCharLine[col])
 
+        
+        integer = int(i, 16)
+        if ccharlength == 0: raise Exception
+        length = 13
+        currentCharLine = f'{integer:0>16b}'
+       
+        currentCharLineList = []
+        for i in range(len(currentCharLine)):
+            currentCharLineList.append(int(currentCharLine[i]))
+
+        cchardata.append(currentCharLineList)
+
+    output["meta"]= {"space": space}
 
 ofile = open("output.json", "w")
-ofile.write(json.dumps({fontname: output}))
+ofile.write(json.dumps({fontname: output})[1:-1]+ ",\n")
 ofile.close()
 print("finished without problems")
